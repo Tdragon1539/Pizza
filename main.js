0719d55f926623a8f80935a78bbd9d3a91432d06
Tdragon1539 <73697550+Tdragon1539@users.noreply.github.com>
2021-03-30 18:53:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function getMenu(){
     var htmldata;
     htmldata="<ol class='menulist'>"
 menu_list_array.sort();
-for(var i=0;<menu_list_array>;i++){
+for(var i=0;i<menu_list_array.length;i++){
     htmldata=htmldata+ '<li>' + menu_list_array[i]+'</li>'
 }
 htmldata=htmldata+ "</ol>"
